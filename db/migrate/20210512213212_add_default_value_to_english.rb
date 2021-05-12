d6b19005fff99e3335b118a507bb054b53f2cbb7
ianbelfatto <ibelfatto@gmail.com>
2021-05-12 17:42:00 -0400

    adds migrations to the Movie and Actor models

---
+++ db/migrate/20210512213212_add_default_value_to_english.rb
@@ -0,0 +1,6 @@
+class AddDefaultValueToEnglish < ActiveRecord::Migration[6.1]
+  def change
+    change_column :movies, :english, 'boolean USING CAST(english AS boolean)'
+    change_column_default :movies, :english, true
+  end
+end
